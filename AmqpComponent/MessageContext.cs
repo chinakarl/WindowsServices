c5ff1b7e9c3770773e2925ecd656273994a35258
zhaihaixinag <zhaihaixinag@beisen.com>
2017-11-03 14:03:49 +0800

    component

---
+++ AmqpComponent/MessageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace AmqpComponent
+{
+    /// <summary>
+    /// 消息上下文
+    /// </summary>
+    public class MessageContext
+    {
+        public IConnection ListenConnection { get; set; }
+
+        public IModel ListenCannel { get; set; }
+    }
+}
